d5d805ec3570860a54f2c364b3339c698f1966d0
ahmet uygun <ahmetuygun140@gmail.com>
2021-10-19 00:06:15 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,6 @@
+let konutkredileri = ["konut kredisi", "emlak konut kredisi", "kamu konut kredisi"]
+console.log("<ul>")
+for (let i = 0; i < konutkredileri.length; i++) {
+    console.log("<li>" + konutkredileri[i] + "</li>")
+}
+console.log("</ul>")
